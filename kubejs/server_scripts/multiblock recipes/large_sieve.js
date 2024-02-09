48ba8f014e66cf5c11426e296b391b2f5f7632cf
trulyno <78795220+trulyno@users.noreply.github.com>
2024-02-09 09:24:58 +0200

    Large sieve fix(issues found by AncientSkies)

--- kubejs/server_scripts/multiblock recipes/large_sieve.js
+++ kubejs/server_scripts/multiblock recipes/large_sieve.js
@@ -24,7 +24,7 @@ ServerEvents.recipes(event => {
             '64x gtceu:crushed_emerald_ore', '64x gtceu:crushed_saltpeter_ore', '64x gtceu:crushed_ruby_ore', '64x gtceu:crushed_realgar_ore', 
             '64x gtceu:crushed_rock_salt_ore', '64x gtceu:crushed_salt_ore', '64x gtceu:crushed_certus_quartz_ore', '64x gtceu:crushed_sapphire_ore',
             '64x gtceu:crushed_monazite_ore', '64x gtceu:crushed_topaz_ore', '64x gtceu:crushed_yellow_garnet_ore', '64x gtceu:crushed_red_garnet_ore', 
-            '64x gtceu:crushed_apatite_ore', '64x gtceu:crushed_topaz_ore', '64x gtceu:crushed_garnet_sand_ore')
+            '64x gtceu:crushed_apatite_ore', '64x gtceu:crushed_blue_topaz_ore', '64x gtceu:crushed_garnet_sand_ore')
         .duration(400)
         .EUt(6400);
 
@@ -34,8 +34,8 @@ ServerEvents.recipes(event => {
         .duration(400)
         .EUt(6400);
 
-    event.recipes.gtceu.large_sieve('blackstone_sieving')
-        .itemInputs('64x exnihilosequentia:blackstone')
+        event.recipes.gtceu.large_sieve('blackstone_sieving')
+        .itemInputs('64x exnihilosequentia:crushed_blackstone')
         .itemOutputs('64x gtceu:crushed_stibnite_ore', '64x gtceu:crushed_galena_ore', '64x gtceu:crushed_pentlandite_ore', '64x gtceu:crushed_bornite_ore', 
             '64x gtceu:crushed_cobaltite_ore', '64x gtceu:crushed_chromite_ore', '64x gtceu:crushed_beryllium_ore', '64x gtceu:crushed_pitchblende_ore', 
             '64x gtceu:crushed_ilmenite_ore', '64x gtceu:crushed_molybdenite_ore', '64x gtceu:crushed_bastnasite_ore', '64x gtceu:crushed_barite_ore', 
