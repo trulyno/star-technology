a09b1358d3f7379ed0444dc60ab73d29bd14c8dc
HamakNaDrzewie <159724861+HamakNaDrzewie@users.noreply.github.com>
2024-02-12 23:39:37 +0100

    Update elements and materials.js

--- kubejs/startup_scripts/elements and materials.js
+++ kubejs/startup_scripts/elements and materials.js
@@ -1384,4 +1384,59 @@ materialRegistry(event => {
         .components('1x isovol', '2x fluorine')
         .color(0x9900ff)
         .flags(no_decomp);
+
+    // PEEK plastic Line
+
+    event.create('disodium_salt_of_hydroquinone')
+        .dust()
+        .color(0xf2f2ff)
+        .flags(no_decomp);
+
+    event.create('hydroquinone')
+        .dust()
+        .components('6x carbon','6x hydrogen','2x oxygen')
+        .color(0xf2f2ff);
+
+    event.create('carbon_acid')
+        .gas()
+        .components('2x hydrogen','1x carbon','3x oxygen')
+        .color(0x333333);
+
+    event.create('fluorobenzene')
+        .fluid()
+        .components('6x carbon','5x hydrogen','1x fluorine')
+        .color(0xffffff);
+
+    event.create('4_fluorobenzoyl_chloride')
+        .fluid()
+        .components('7x carbon','4x hydrogen','1x chlorine','1x fluorine','1x oxygen')
+        .color(0xffffe0);
+    
+    event.create('benzoyl_chloride')
+        .fluid()
+        .components('7x carbon','5x hydrogen','1x chlorine','1x oxygen')
+        .color(0xfffadf);
+
+    event.create('benzotrichloride')
+        .fluid()
+        .components('7x carbon','5x hydrogen','3x chlorine')
+        .color(0xfff8dc);
+
+    event.create('toulene')
+        .fluid()
+        .components('6x carbon','5x hydrogen','1x methane')
+        .color(0x322a18);
+
+    event.create('44_difluorobenzophenone') //naming like this: 4_4_di... will make kubejs go error to annoy you :)
+        .dust()
+        .components('13x carbon','8x hydrogen','1x oxygen','2x fluorine') 
+        .color(0xeee1c9)
+        .flags(no_decomp);
+    
+    event.create('polyether_ether_ketone')
+        .polymer()
+        .components('19x carbon','12x hydrogen','3x oxygen') 
+        .color(0xccbba7)
+        .flags(no_decomp);
+
 });
