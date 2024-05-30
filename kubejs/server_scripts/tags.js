843b376f4935a554c88ad5028b9407c1e9196bfb
trulyno <78795220+trulyno@users.noreply.github.com>
2024-05-30 13:12:14 +0300

    temporary fix for treated wood sticks

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -4,4 +4,5 @@ ServerEvents.tags('item', event => {
     event.add('gtceu:circuits/uhv', 'kubejs:draconic_wetware_processor_assembly');
     event.add('gtceu:circuits/uev', 'kubejs:draconic_wetware_processor_computer');
     event.add('gtceu:circuits/uiv', 'kubejs:draconic_wetware_processor_mainframe');
+    event.remove('minecraft:planks', 'gtceu:treated_wood_planks');
 });
