d535743c714aa32f8aa40cecf44c3fd0b6ccc0ba
UltraPuPower1 <122467778+UltraPuPower@users.noreply.github.com>
2024-10-31 20:16:56 +0100

    1.20 sync

--- kubejs/server_scripts/multiblock recipes/chemical_plant.js
+++ kubejs/server_scripts/multiblock recipes/chemical_plant.js
@@ -2,7 +2,7 @@
 ServerEvents.recipes(event => {
 
     event.recipes.gtceu.assembly_line('chemical_plant_controller')
-        .itemInputs('gtceu:zpm_machine_hull', '4x gtceu:zpm_electric_motor', 'gtceu:naquadah_alloy_rotor', '2x gtceu:niobium_titanium_large_fluid_pipe', '4x #gtceu:circuits/zpm')
+        .itemInputs('gtceu:zpm_machine_hull', '4x gtceu:zpm_electric_motor', 'gtceu:naquadah_alloy_rotor', '2x gtceu:niobium_titanium_large_fluid_pipe', '4x #gtceu:circuits/uv')
         .inputFluids('gtceu:soldering_alloy 1872', 'gtceu:naquadria 288')
         .itemOutputs('gtceu:chemical_plant')
         .duration(1200)
