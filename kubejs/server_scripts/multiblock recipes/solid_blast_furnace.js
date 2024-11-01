4cfe6d41a299089974ddb23e3f0815c7c86e343c
ssoaper <164716081+ssoaper@users.noreply.github.com>
2024-10-31 17:35:19 -0700

    Alternative Steel recipes

--- kubejs/server_scripts/multiblock recipes/solid_blast_furnace.js
+++ kubejs/server_scripts/multiblock recipes/solid_blast_furnace.js
@@ -13,4 +13,27 @@ ServerEvents.recipes(event => {
         B: 'gtceu:solid_machine_casing',
         F: '#forge:tools/screwdrivers'
     });
+
+    event.recipes.gtceu.solid_blast_furnace('sbf/magnetite_dust/silicon')
+        .duration(600)
+        .itemInputs(
+            '4x gtceu:magnetite_dust',
+            '2x gtceu:silicon_dust',
+            '1x #kubejs:sbf_fuels'
+        )
+        .itemOutputs(
+            '2x minecraft:iron_ingot',
+            '6x gtceu:silicon_dioxide_dust'
+        )
+    event.recipes.gtceu.solid_blast_furnace('sbf/iron_ingot/silicon_dioxide')
+        .duration(300)
+        .itemInputs(
+            'minecraft:iron_ingot',
+            '3x gtceu:silicon_dioxide_dust',
+            '1x #kubejs:sbf_fuels'
+        )
+        .itemOutputs(
+            'gtceu:steel_ingot',
+            'gtceu:silicon_dust'
+        )
 });
