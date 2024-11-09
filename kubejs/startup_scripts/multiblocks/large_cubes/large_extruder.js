bfce6c13e6768de9fd7205fe57c1c616eebc06e5
Suprateek Nag <tipunag12@gmail.com>
2024-11-09 09:21:35 +0530

    fix large cube textures

--- kubejs/startup_scripts/multiblocks/large_cubes/large_extruder.js
+++ kubejs/startup_scripts/multiblocks/large_cubes/large_extruder.js
@@ -17,5 +17,5 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .where(' ', Predicates.air())
             .build())
         .workableCasingRenderer("kubejs:block/casings/beryllium-aluminium_casing",
-        "gtceu:block/multiblock/electric_blast_furnace", false);
+        "gtceu:block/machines/extruder", false);
 });
--- kubejs/startup_scripts/multiblocks/large_cubes/large_macerator.js
+++ kubejs/startup_scripts/multiblocks/large_cubes/large_macerator.js
@@ -17,5 +17,5 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .where(' ', Predicates.air())
             .build())
         .workableCasingRenderer("kubejs:block/casings/blue_steel_casing",
-        "gtceu:block/multiblock/electric_blast_furnace", false);
+        "gtceu:block/machines/macerator", false);
 });
