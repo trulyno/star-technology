24975c3342795184d5addf500ae3f5bdc1b1f62b
VictorStroganoff <victorstroganoff123@gmail.com>
2024-11-13 23:03:07 +0200

    Update hard mode recipiee changes.js
    Now it works...

--- kubejs/server_scripts/hardmode_server_scripts/hard mode recipiee changes.js
+++ kubejs/server_scripts/hardmode_server_scripts/hard mode recipiee changes.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
     
-        event.recipes.create.mixing('4x gtceu:galvanized_steel_ingot', ['3x gtceu:steel_ingot', '144x gtceu:zinc']).heatRequirement('lowheated');
+event.recipes.create.mixing(['4x gtceu:galvanized_steel_ingot'], ['3x gtceu:steel_ingot', Fluid.of('gtceu:zinc',144)]).heatRequirement('lowheated');
         
         
 
