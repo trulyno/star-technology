7faaa6c254fbe92d0287d987587fe495de0ab6ab
73mm1n <128571861+73mm1n@users.noreply.github.com>
2024-11-13 23:08:39 +0300

    Add files via upload

---
+++ kubejs/server_scripts/hardmode_server_scripts/hard mode recipiee changes.js
@@ -0,0 +1,13 @@
+ServerEvents.recipes(event => {
+    
+        event.recipes.create.mixing('1x gtceu:galvanized_steel_ingot', ['1x gtceu:steel_dust', 'gtceu:zinc_ingot']).heatRequirement('lowheated');
+        
+        
+
+
+
+
+
+
+
+})
