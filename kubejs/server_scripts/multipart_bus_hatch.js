3611615bfe7bc646df28dfb75c22f77840665c10
UltraPuPower1 <ultrapupower1@gmail.com>
2024-11-21 15:51:43 +0100

    crafting hatches like in the assembler

--- kubejs/server_scripts/multipart_bus_hatch.js
+++ kubejs/server_scripts/multipart_bus_hatch.js
@@ -1,40 +1,68 @@
 // Readds Multipart bus/hatch recipes removed in 1.20.1 GT 
-// for LV, ULV and MV tiers with parity to the 1.19.2 BETA 10 hotfix 4 recipes.
+// for ULV and LV tiers with parity to the GTceuM assembler recipes.
 ServerEvents.recipes(event => {
-    ['ulv', 'lv', 'mv'].forEach(tier => {
-        // Busses
-        event.shaped(Item.of(`gtceu:${tier}_input_bus`), [
-            ' C ',
-            ' M ',
-            '   '
-        ], {
-            C: '#forge:chests/wooden',
-            M: `gtceu:${tier}_machine_hull`
-        });
-        event.shaped(Item.of(`gtceu:${tier}_output_bus`), [
-            '   ',
-            ' M ',
-            ' C '
-        ], {
-            C: '#forge:chests/wooden',
-            M: `gtceu:${tier}_machine_hull`
-        });
-        // Hatches
-        event.shaped(Item.of(`gtceu:${tier}_input_hatch`), [
-            ' G ',
-            ' M ',
-            '   '
-        ], {
-            G: '#forge:glass',
-            M: `gtceu:${tier}_machine_hull`
-        });
-        event.shaped(Item.of(`gtceu:${tier}_output_hatch`), [
-            '   ',
-            ' M ',
-            ' G '
-        ], {
-            G: '#forge:glass',
-            M: `gtceu:${tier}_machine_hull`
-        });
+    // ULV
+    // Busses
+    event.shaped(Item.of('gtceu:ulv_input_bus'), [
+        ' C ',
+        ' M ',
+        '   '], {
+        C: '#forge:chests/wooden',
+        M: 'gtceu:ulv_machine_hull'
     });
-})
+    event.shaped(Item.of('gtceu:ulv_output_bus'), [
+        '   ',
+        ' M ',
+        ' C '], {
+        C: '#forge:chests/wooden',
+        M: 'gtceu:ulv_machine_hull'
+    });
+    // Hatches
+    event.shaped(Item.of('gtceu:ulv_input_hatch'), [
+        ' G ',
+        ' M ',
+        '   '], {
+        G: '#forge:glass',
+        M: 'gtceu:ulv_machine_hull'
+    });
+    event.shaped(Item.of('gtceu:ulv_output_hatch'), [
+        '   ',
+        ' M ',
+        ' G '], {
+        G: '#forge:glass',
+        M: 'gtceu:ulv_machine_hull'
+    });
+
+    // LV
+    // Busses
+    event.shaped(Item.of('gtceu:ulv_input_bus'), [
+        ' C ',
+        ' M ',
+        '   '], {
+        C: 'gtceu:wood_crate',
+        M: 'gtceu:ulv_machine_hull'
+    });
+    event.shaped(Item.of('gtceu:ulv_output_bus'), [
+        '   ',
+        ' M ',
+        ' C '], {
+        C: 'gtceu:wood_crate',
+        M: 'gtceu:ulv_machine_hull'
+    });
+    // Hatches
+    event.shaped(Item.of('gtceu:ulv_input_hatch'), [
+        ' G ',
+        ' M ',
+        '   '], {
+        G: 'gtceu:wood_drum',
+        M: 'gtceu:ulv_machine_hull'
+    });
+    event.shaped(Item.of('gtceu:ulv_output_hatch'), [
+        '   ',
+        ' M ',
+        ' G '], {
+        G: 'gtceu:wood_drum',
+        M: 'gtceu:ulv_machine_hull'
+    });
+
+});
