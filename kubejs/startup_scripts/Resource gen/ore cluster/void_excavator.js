53b954b456c9e5524dd6789d1b1ecda49d643b8d
UltraPuPower1 <ultrapupower1@gmail.com>
2024-12-20 10:02:00 +0100

    parallel void mining

--- kubejs/startup_scripts/Resource gen/ore cluster/void_excavator.js
+++ kubejs/startup_scripts/Resource gen/ore cluster/void_excavator.js
@@ -1,35 +1,36 @@
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('void_excavator')
         .category('void_excavator')
-        .setMaxIOSize(0, 9, 1, 2)
-        .setSound(GTSoundEntries.MACERATOR);
+        .setMaxIOSize(1, 9, 1, 2)
+        .setSound(GTSoundEntries.MINER);
 });
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('void_excavator', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
         .recipeType('void_excavator')
+        .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH])
+        .appearanceBlock(GTBlocks.CASING_TUNGSTENSTEEL_ROBUST)
         .pattern(definition => FactoryBlockPattern.start()
         .aisle('#SFS#', '##E##', '##E##', '##E##', '#####', '#####', '#####', '#####', '#####', '#####', '#####', '#####')
-        .aisle('SSSSS', '#EGE#', '#EGE#', '##E##', '##E##', '##E##', '##E##', '#####', '#####', '#####', '#####', '#####')
-        .aisle('FSSSF', 'EGAGE', 'EGAGE', 'EEPEE', '#EPE#', '#EPE#', '#EPE#', '##E##', '##E##', '##E##', '##E##', '##E##')
-        .aisle('SSSSS', '#EGE#', '#EGE#', '##E##', '##E##', '##E##', '##E##', '#####', '#####', '#####', '#####', '#####')
+        .aisle('SSSSS', '#ERE#', '#EGE#', '##E##', '##E##', '##E##', '##E##', '#####', '#####', '#####', '#####', '#####')
+        .aisle('FSSSF', 'ERGRE', 'EGGGE', 'EEGEE', '#EGE#', '#EGE#', '#EGE#', '##E##', '##E##', '##E##', '##E##', '##E##')
+        .aisle('SSSSS', '#ERE#', '#EGE#', '##E##', '##E##', '##E##', '##E##', '#####', '#####', '#####', '#####', '#####')
         .aisle('#SCS#', '##E##', '##E##', '##E##', '#####', '#####', '#####', '#####', '#####', '#####', '#####', '#####')
         .where('C', Predicates.controller(Predicates.blocks(definition.get())))
-        .where('S', Predicates.blocks('gtceu:solid_machine_casing').setMinGlobalLimited(8)
+        .where('S', Predicates.blocks('gtceu:robust_machine_casing').setMinGlobalLimited(8)
             .or(Predicates.abilities(PartAbility.IMPORT_ITEMS).setMaxGlobalLimited(1))
             .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(1))
             .or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setMaxGlobalLimited(2))
             .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setMaxGlobalLimited(2))
             .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
             .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(2)))
-        .where('F', Predicates.blocks('gtceu:steel_firebox_casing'))
-        .where('A', Predicates.blocks('gtceu:soul_infused_block'))
-        .where('E', Predicates.blocks('gtceu:steel_frame'))
-        .where('G', Predicates.blocks('gtceu:steel_gearbox'))
-        .where('P', Predicates.blocks('gtceu:steel_machine_casing'))
+        .where('F', Predicates.blocks('gtceu:tungstensteel_firebox_casing'))
+        .where('R', Predicates.blocks('gtceu:rtm_alloy_coil_block'))
+        .where('E', Predicates.blocks('gtceu:tungsten_steel_frame'))
+        .where('G', Predicates.blocks('gtceu:tungstensteel_gearbox'))
         .where('#', Predicates.any())
             .build())
-        .workableCasingRenderer("gtceu:block/casings/solid/machine_casing_solid_steel",
-        "gtceu:block/multiblock/electric_blast_furnace", false);
+        .workableCasingRenderer("gtceu:block/casings/solid/machine_casing_robust_tungstensteel",
+        "gtceu:block/multiblock/large_miner", false);
 });
