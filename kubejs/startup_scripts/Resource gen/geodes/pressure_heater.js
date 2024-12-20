693d2342b0ac624684cdd928484a0418e346ccb5
UltraPuPower1 <ultrapupower1@gmail.com>
2024-12-20 10:01:34 +0100

    parallel geode cracking

--- kubejs/startup_scripts/Resource gen/geodes/pressure_heater.js
+++ kubejs/startup_scripts/Resource gen/geodes/pressure_heater.js
@@ -2,15 +2,17 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('pressure_heater')
         .category('pressure_heater')
         .setMaxIOSize(2, 2, 0, 0)
-        .setSound(GTSoundEntries.MACERATOR);
+        .setSound(GTSoundEntries.FIRE);
 });
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
-    event.create('pressure_heater', 'multiblock')
+    event.create('pressure_cracker', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
         .recipeType('pressure_heater')
+        .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH])
+        .appearanceBlock(GTBlocks.CASING_INVAR_HEATPROOF)
         .pattern(definition => FactoryBlockPattern.start()
-            .aisle('#H###H#', 'HH###HH', 'HHHSHHH', 'HHSISHH', 'HHHSHHH', 'HH###HH', '#H###H#')
+            .aisle('#H###H#', 'HH###HH', 'HHSSSHH', 'HSSISSH', 'HHSSSHH', 'HH###HH', '#H###H#')
             .aisle('#HHHHH#', 'HHHHHHH', 'FPBBBPF', 'FPBPBPF', 'FPBBBPF', 'HHHHHHH', '#HHHHH#')
             .aisle('#HGGGH#', 'HHBBBHH', 'HB###BH', 'HB#P#BH', 'HB###BH', 'HHBBBHH', '#HGGGH#')
             .aisle('#HGGGH#', 'HHBBBHH', 'GB###BG', 'GB#P#BG', 'GB###BG', 'HHBBBHH', '#HGGGH#')
@@ -18,7 +20,7 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .aisle('#HGGGH#', 'HHBBBHH', 'GB###BG', 'GB#P#BG', 'GB###BG', 'HHBBBHH', '#HGGGH#')
             .aisle('#HGGGH#', 'HHBBBHH', 'HB###BH', 'HB#P#BH', 'HB###BH', 'HHBBBHH', '#HGGGH#')
             .aisle('#HHCHH#', 'HHHHHHH', 'FPBBBPF', 'FPBPBPF', 'FPBBBPF', 'HHHHHHH', '#HHHHH#')
-            .aisle('#H###H#', 'HH###HH', 'HHHSHHH', 'HHSOSHH', 'HHHSHHH', 'HH###HH', '#H###H#')
+            .aisle('#H###H#', 'HH###HH', 'HHSSSHH', 'HSSOSSH', 'HHSSSHH', 'HH###HH', '#H###H#')
             .where('H', Predicates.blocks('gtceu:heatproof_machine_casing')
                 .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
                 .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(2)))
@@ -26,10 +28,10 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .where('I', Predicates.abilities(PartAbility.IMPORT_ITEMS))
             .where('O', Predicates.abilities(PartAbility.EXPORT_ITEMS))
             .where('G', Predicates.blocks('gtceu:tempered_glass'))
-            .where('F', Predicates.blocks('gtceu:steel_firebox_casing'))     
-            .where('B', Predicates.blocks('gtceu:cupronickel_coil_block'))
-            .where('P', Predicates.blocks('gtceu:steel_pipe_casing'))
-            .where('S', Predicates.blocks('gtceu:steel_machine_casing'))
+            .where('F', Predicates.blocks('gtceu:tungstensteel_firebox_casing'))
+            .where('B', Predicates.blocks('gtceu:nichrome_coil_block'))
+            .where('P', Predicates.blocks('gtceu:tungstensteel_pipe_casing'))
+            .where('S', Predicates.blocks('gtceu:robust_machine_casing'))
             .where('#', Predicates.any())
             .build())
         .workableCasingRenderer("gtceu:block/casings/solid/machine_casing_heatproof",
