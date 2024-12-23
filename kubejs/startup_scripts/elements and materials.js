7c5eda9c63d45ef6891d9f4f27708abbf048a9f0
UltraPuPower1 <ultrapupower1@gmail.com>
2024-12-23 20:10:24 +0100

    move fluids to GT

--- kubejs/startup_scripts/elements and materials.js
+++ kubejs/startup_scripts/elements and materials.js
@@ -1532,3 +1532,75 @@ materialRegistry(event => {
      .iconSet(METALLIC)
      .blastTemp(2200, 'low', VA('mv'), 2000);
 });
+
+//EPSILON Resource Gen stuff
+materialRegistry(event => {
+
+    function liquid(name, color, composition){
+        event.create(name)
+            .fluid()
+            .components(composition)
+            .color(color);
+    };
+
+    liquid('iron_mixture', 0xC42626, '1x mystery');
+    liquid('copper_mixture', 0xC86524, '1x mystery');
+    liquid('quartz_mixture', 0xABC5E0, '1x mystery');
+    liquid('rare_ore_residue', 0x556278, '1x mystery');
+    liquid('raw_ore_slurry', 0x7B8087, '1x mystery');
+    liquid('molten_ore_mixture', 0x575050, '1x mystery');
+
+    /*/coagulated liquids
+    event.create('iron_mixture')
+        .thickTexture(0xC42626)
+        .bucketColor(0xC42626)
+        .displayName('Iron-rich Mixture')
+        .noBlock();
+  
+    event.create('copper_mixture')
+        .thickTexture(0xC86524)
+        .bucketColor(0xC86524)
+        .displayName('Copper-rich Mixture')
+        .noBlock();
+
+    event.create('quartz_mixture')
+        .thickTexture(0xABC5E0)
+        .bucketColor(0xABC5E0)
+        .displayName('Quartz-rich Mixture')
+        .noBlock();
+
+    //rare ore clusters
+    event.create('rare_ore_residue')
+        .thickTexture(0x556278)
+        .bucketColor(0x556278)
+        .displayName('Rare Ore Residue')
+        .noBlock();
+
+    //crystallisation liquids
+    event.create('raw_ore_slurry')
+        .thickTexture(0x7B8087)
+        .bucketColor(0x7B8087)
+        .displayName('Raw Ore Slurry')
+        .noBlock();
+
+    event.create('molten_ore_mixture')
+        .thickTexture(0x575050)
+        .bucketColor(0x575050)
+        .displayName('Molten Ore Mixture')
+        .noBlock();*/
+
+    //molten ores
+    function moltenore(name, color){
+        liquid(`molten_${name}_ore`, color, name);
+    };
+    
+    moltenore('bauxite', 0x18C940);
+    moltenore('pitchblende', 0x13F045);
+    moltenore('molybdenite', 0x9DB3A2);
+    moltenore('ilmenite', 0x630923);
+    moltenore('scheelite', 0xAAB5A8);
+    moltenore('tungstate', 0x89AD82);
+    liquid('molten_sheldonite_ore', 0x89CC7C, '1x mystery');
+    
+
+});
