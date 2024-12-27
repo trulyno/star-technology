ebb87ec8c99853bd86dd536cb62e73bf94a80185
UltraPuPower1 <ultrapupower1@gmail.com>
2024-12-27 11:29:26 +0100

    clean-up

--- kubejs/server_scripts/resource gen/seawater.js
+++ kubejs/server_scripts/resource gen/seawater.js
@@ -1,11 +1,11 @@
 ServerEvents.recipes(event => {
 
     event.recipes.gtceu.centrifuge('sea_water')
-    .inputFluids('exnihilosequentia:sea_water 1500')
-    .outputFluids('gtceu:salt_water 1000')
-    .itemOutputs('gtceu:saltpeter_dust', 'gtceu:rock_salt_dust', 'gtceu:small_borax_dust')
-    .duration(50)
-    .EUt(16);
+        .inputFluids('exnihilosequentia:sea_water 1500')
+        .outputFluids('gtceu:salt_water 1000')
+        .itemOutputs('gtceu:saltpeter_dust', 'gtceu:rock_salt_dust', 'gtceu:small_borax_dust')
+        .duration(50)
+        .EUt(16);
 
     //large barrel
     event.recipes.gtceu.large_barrel('sea_water')
@@ -13,4 +13,5 @@ ServerEvents.recipes(event => {
         .inputFluids('minecraft:water 1000')
         .outputFluids('exnihilosequentia:sea_water 1000')
         .duration(80);
-})
+
+});
