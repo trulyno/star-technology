61d3836f81910eda42330386cf10c0027dc1343e
UltraPuPower1 <ultrapupower1@gmail.com>
2024-12-27 21:26:51 +0100

    move some stuff around

--- kubejs/startup_scripts/Resource gen/item_block_registry.js
+++ kubejs/startup_scripts/Resource gen/item_block_registry.js
@@ -67,6 +67,27 @@ StartupEvents.registry('item', event => {
 
 StartupEvents.registry('block', event => {
 
+    //Coagulated blocks
+    ['iron', 'copper', 'quartz'].forEach(type => {
+        event.create(`coagulated_${type}_mixture`)
+            .displayName(`Coagulated ${type.charAt(0).toUpperCase() + type.slice(1)} Mixture`)
+            .hardness(5)
+            .resistance(5)
+            .material('stone')
+            .requiresTool(true)
+            .tagBlock("mineable/pickaxe")
+            .textureAll(`kubejs:block/resource_gen/coagulated_${type}_mixture`);
+    });
+    
+    event.create('coagulated_lava')
+        .displayName('Coagulated Lava')
+        .hardness(5)
+        .resistance(5)
+        .material('stone')
+        .requiresTool(true)
+        .tagBlock("mineable/pickaxe")
+        .textureAll('kubejs:block/resource_gen/coagulated_lava');
+
     event.create('reflective_casing')
         .hardness(10)
         .resistance(1)
