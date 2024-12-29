fc4e3eec1e3f094cafec3d2fb46dd9501ef8e2da
UltraPuPower1 <ultrapupower1@gmail.com>
2024-12-29 14:00:54 +0100

    finish greens

--- kubejs/client_scripts/renaming_items.js
+++ kubejs/client_scripts/renaming_items.js
@@ -1,4 +1,3 @@
-
 ClientEvents.lang('en_us', event => {
     event.renameItem("createdieselgenerators:biodiesel_bucket", "Crude Bio Diesel Bucket");
     event.add('fluid.createdieselgenerators:biodiesel', 'Crude Bio Diesel');
