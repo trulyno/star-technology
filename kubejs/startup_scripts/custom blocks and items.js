a35ab5d5c6ddb8eb9613051503fba1999eb47dee
UltraPuPower1 <ultrapupower1@gmail.com>
2025-01-11 14:45:07 +0100

    pipe casing and function driven generation

--- kubejs/startup_scripts/custom blocks and items.js
+++ kubejs/startup_scripts/custom blocks and items.js
@@ -894,16 +894,21 @@ StartupEvents.registry('block', event => {
 //         .tagBlock('minecraft:needs_iron_tool')
 //         .textureAll('kubejs:block/resource_gen/reflective_casing');
 
-    event.create('enriched_naquadah_machine_casing')
-        .displayName('Enriched Naquadah Casing')
-        .hardness(5)
-        .resistance(10)
-        .lightLevel(0)
-        .soundType('metal')
-        .requiresTool(true)
-        .tagBlock("mineable/pickaxe")
-        .tagBlock('minecraft:needs_iron_tool')
-        .textureAll('kubejs:block/casings/naquadah/casing');
+    function naq_casing(id, name, texture){
+        event.create(id)
+            .displayName(name)
+            .hardness(5)
+            .resistance(10)
+            .lightLevel(0)
+            .soundType('metal')
+            .requiresTool(true)
+            .tagBlock("mineable/pickaxe")
+            .tagBlock('minecraft:needs_iron_tool')
+            .textureAll(`kubejs:block/casings/naquadah/${texture}`);
+        };
+
+    naq_casing('enriched_naquadah_machine_casing', 'Enriched Naquadah Casing', 'casing');
+    naq_casing('enriched_naquadah_pipe_casing', 'Enriched Naquadah Pipe Casing', 'pipe_casing');
 
 });
 
