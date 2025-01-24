8ad398a5570f3978c4e5cc0e41e5068c342f4dcd
KillLaAqua <KillLaAqua@gmail.com>
2025-01-24 15:25:11 -0600

    Recipe Adjustments and Greenhouse Fix

--- kubejs/server_scripts/resource gen/mechanical_sieve.js
+++ kubejs/server_scripts/resource gen/mechanical_sieve.js
@@ -83,7 +83,7 @@ ServerEvents.recipes(event => {
             .itemInputs(`${input}`)
             .itemOutputs(outputs)
             .duration(400)
-            .EUt(GTValues.VA[GTValues.IV])
+            .EUt(GTValues.VA[GTValues.EV])
 
     }
 
