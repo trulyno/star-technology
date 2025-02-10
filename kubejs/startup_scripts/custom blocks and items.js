313f581b80cbd1bf3e39e54c459579fa07803748
TOBI6HANA <140648632+TOBI6HANA@users.noreply.github.com>
2025-02-10 13:08:36 +0100

    Made KubeJS wafers and chips have gray description instead on white
    For parity with base gt

--- kubejs/startup_scripts/custom blocks and items.js
+++ kubejs/startup_scripts/custom blocks and items.js
@@ -59,32 +59,32 @@ StartupEvents.registry('item', event => {
 
     event.create('3d_nand_chip')
         .displayName('3D-NAND Memory Chip Array')
-        .tooltip('Complex NAND Logic Gate')
+        .tooltip(Text.gray('Complex NAND Logic Gate'))
         .texture('kubejs:item/chips_n_wafers/3d_nand')
 
     event.create('3d_nor_chip')
         .displayName('3D-NOR Memory Chip Array')
-        .tooltip('Complex NOR Logic Gate')
+        .tooltip(Text.gray('Complex NOR Logic Gate'))
         .texture('kubejs:item/chips_n_wafers/3d_nor')
 
     event.create('qram_wafer')
         .displayName('qRAM Wafer')
-        .tooltip('Raw Quantum Memory')
+        .tooltip(Text.gray('Raw Quantum Memory'))
         .texture('kubejs:item/chips_n_wafers/qram_wafer')
 
     event.create('qram_chip')
         .displayName('qRAM Chip')
-        .tooltip('Quantum Random Access Memory')
+        .tooltip(Text.gray('Quantum Random Access Memory'))
         .texture('kubejs:item/chips_n_wafers/qram_chip')
 
     event.create('uepic_wafer')
         .displayName('UEPIC Wafer')
-        .tooltip('Raw Ultra Excessive Power Circuit')
+        .tooltip(Text.gray('Raw Ultra Excessive Power Circuit'))
         .texture('kubejs:item/chips_n_wafers/uepic_wafer')
 
     event.create('uepic_chip')
         .displayName('UEPIC Chip')
-        .tooltip('Ultra Excessive Power IC')
+        .tooltip(Text.gray('Ultra Excessive Power IC'))
         .texture('kubejs:item/chips_n_wafers/uepic_chip')
     
     event.create('draconic_wetware_circuit_board')
