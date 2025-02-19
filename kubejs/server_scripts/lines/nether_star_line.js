1a1f8d29ccf6c9c870e9ceb65f923839874b410f
KillLaAqua <KillLaAqua@gmail.com>
2025-02-19 15:56:36 -0600

    Better Powders and Cleanup

--- kubejs/server_scripts/lines/nether_star_line.js
+++ kubejs/server_scripts/lines/nether_star_line.js
@@ -1,83 +1,37 @@
 
 ServerEvents.recipes(event => {
 
-    event.recipes.gtceu.chemical_reactor('blizz_dust')
-        .itemInputs('gtceu:cobalt_dust')
-        .inputFluids('gtceu:fluorine 1000')
-        .itemOutputs('thermal:blizz_powder')
-        .duration(200)
-        .EUt(7680);
+    [
+        {powder: 'blizz', item: 'cobalt_dust', fluid: 'fluorine', multiplier: 16, element: 'ice'},
+        {powder: 'blitz', item: 'platinum_dust', fluid: 'deuterium', multiplier: 4, element: 'lightning'},
+        {powder: 'basalz', item: 'small_rhodium_dust', fluid: 'helium', multiplier: 1, element: 'earth'}
+    ].forEach(type=>{
+        event.recipes.gtceu.chemical_reactor(`${type.powder}_dust`)
+            .itemInputs(`gtceu:${type.item}`)
+            .inputFluids(`gtceu:${type.fluid} 1000`)
+            .itemOutputs(`${type.multiplier}x thermal:${type.powder}_powder`)
+            .duration(200)
+            .EUt(480*type.multiplier);
 
-    event.recipes.gtceu.large_chemical_reactor('blizz_dust')
-        .itemInputs('gtceu:cobalt_dust')
-        .inputFluids('gtceu:fluorine 1000')
-        .itemOutputs('thermal:blizz_powder')
-        .duration(200)
-        .EUt(7680);
+        event.recipes.gtceu.large_chemical_reactor(`${type.powder}_dust`)
+            .itemInputs(`gtceu:${type.item}`)
+            .inputFluids(`gtceu:${type.fluid} 1000`)
+            .itemOutputs(`${type.multiplier}x thermal:${type.powder}_powder`)
+            .duration(200)
+            .EUt(480*type.multiplier);
 
-    event.recipes.gtceu.chemical_reactor('blitz_dust')
-        .itemInputs('gtceu:platinum_dust')
-        .inputFluids('gtceu:deuterium 1000')
-        .itemOutputs('thermal:blitz_powder')
-        .duration(200)
-        .EUt(1920);
+        event.recipes.gtceu.macerator(`${type.powder}_powder`)
+            .itemInputs(`thermal:${type.powder}_rod`)
+            .itemOutputs(`4x thermal:${type.powder}_powder`)
+            .duration(88)
+            .EUt(2);
 
-     event.recipes.gtceu.large_chemical_reactor('blitz_dust')
-        .itemInputs('gtceu:platinum_dust')
-        .inputFluids('gtceu:deuterium 1000')
-        .itemOutputs('thermal:blitz_powder')
-        .duration(200)
-        .EUt(1920);
-
-    event.recipes.gtceu.chemical_reactor('basalz_dust')
-        .itemInputs('gtceu:rhodium_dust')
-        .inputFluids('gtceu:helium 1000')
-        .itemOutputs('thermal:basalz_powder')
-        .duration(200)
-        .EUt(480);
-
-     event.recipes.gtceu.large_chemical_reactor('basalz_dust')
-        .itemInputs('gtceu:rhodium_dust')
-        .inputFluids('gtceu:helium 1000')
-        .itemOutputs('thermal:basalz_powder')
-        .duration(200)
-        .EUt(480);
-
-    event.recipes.gtceu.macerator('basalz_powder')
-        .itemInputs('thermal:basalz_rod')
-        .itemOutputs('4x thermal:basalz_powder')
-        .duration(88)
-        .EUt(2);
-
-    event.recipes.gtceu.macerator('blizz_powder')
-        .itemInputs('thermal:blizz_rod')
-        .itemOutputs('4x thermal:blizz_powder')
-        .duration(88)
-        .EUt(2);
-
-    event.recipes.gtceu.macerator('blitz_powder')
-        .itemInputs('thermal:blitz_rod')
-        .itemOutputs('4x thermal:blitz_powder')
-        .duration(88)
-        .EUt(2);
-
-    event.recipes.gtceu.mixer('earth_charge')
-        .itemInputs('#forge:dusts/coal', 'minecraft:gunpowder', 'thermal:basalz_powder')
-        .itemOutputs('3x thermal:earth_charge')
-        .duration(100)
-        .EUt(30);
-
-    event.recipes.gtceu.mixer('ice_charge')
-        .itemInputs('#forge:dusts/coal', 'minecraft:gunpowder', 'thermal:blizz_powder')
-        .itemOutputs('3x thermal:ice_charge')
-        .duration(100)
-        .EUt(30);
-
-    event.recipes.gtceu.mixer('lightning_charge')
-        .itemInputs('#forge:dusts/coal', 'minecraft:gunpowder', 'thermal:blitz_powder')
-        .itemOutputs('3x thermal:lightning_charge')
-        .duration(100)
-        .EUt(30);
+        event.recipes.gtceu.mixer(`${type.element}_charge`)
+            .itemInputs('#forge:dusts/coal', 'minecraft:gunpowder', `thermal:${type.powder}_powder`)
+            .itemOutputs(`3x thermal:${type.element}_charge`)
+            .duration(100)
+            .EUt(30);
+    });
 
     //new stuff
     [
