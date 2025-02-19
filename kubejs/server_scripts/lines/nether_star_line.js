8bee499fa52bf36ca5b515f4ad9c8486a1aac4e7
UltraPuPower1 <ultrapupower1@gmail.com>
2025-02-19 20:37:30 +0100

    finish functional line, textures needed

--- kubejs/server_scripts/lines/nether_star_line.js
+++ kubejs/server_scripts/lines/nether_star_line.js
@@ -80,6 +80,34 @@ ServerEvents.recipes(event => {
         .EUt(30);
 
     //new stuff
+    [
+        {element: 'fire', mod: 'minecraft', powder: 'blaze'},
+        {element: 'ice', mod: 'thermal', powder: 'blizz'},
+        {element: 'lightning', mod: 'thermal', powder: 'blitz'},
+        {element: 'earth', mod: 'thermal', powder: 'basalz'}
+      ].forEach(shard => {
+        event.recipes.gtceu.extractor(`liquid_${shard.powder}`)
+          .itemInputs(`${shard.mod}:${shard.powder}_powder`)
+          .outputFluids(`gtceu:${shard.powder}_essence 144`)
+          .duration(22)
+          .EUt(30);
+
+        event.recipes.gtceu.autoclave(`${shard.element}_infused_shard_shard`)
+          .itemInputs('kubejs:energized_nether_star_shard')
+          .inputFluids(`gtceu:${shard.powder} 720`)
+          .chancedOutput(`kubejs:${shard.element}_infused_shard`, 8000, 500)
+          .duration(480)
+          .EUt(2048);
+      
+        event.recipes.gtceu.autoclave(`${shard.element}_infused_shard_charge`)
+          .itemInputs(`4x ${shard.mod}:${shard.element}_charge`)
+          .inputFluids(`gtceu:${shard.powder} 720`)
+          .chancedOutput(`kubejs:${shard.element}_infused_shard`, 5000, -1000)
+          .duration(480)
+          .EUt(2048);
+      
+      });
+
     event.shaped('kubejs:star_casting_mold', [
         ' F ',
         ' M ',
@@ -100,7 +128,7 @@ ServerEvents.recipes(event => {
         event.recipes.gtceu.implosion_compressor(`nether_star_${name}`)
             .itemInputs('kubejs:impure_nether_star', explosive)
             .itemOutputs('minecraft:nether_star')
-            .chancedOutput('gtceu:ashes', 2500, 0)
+            .chancedOutput('gtceu:dark_ash_dust', 2500, 0)
             .duration(20)
             .EUt(30);
     };
@@ -111,13 +139,13 @@ ServerEvents.recipes(event => {
     implosion('powderbarrel', '8x gtceu:powderbarrel');
 
     event.recipes.gtceu.forge_hammer('nether_star_shard')
-        .itemInputs('minecraft:_nether_star')
-        .itemOutputs('5x mysticalagradittions:nether_star_shard')
+        .itemInputs('minecraft:nether_star')
+        .itemOutputs('5x mysticalagradditions:nether_star_shard')
         .duration(300)
         .EUt(512);
 
     event.recipes.gtceu.polarizer('energized_nether_star_shard')
-        .itemInputs('mysticalagradittions:nether_star_shard')
+        .itemInputs('mysticalagradditions:nether_star_shard')
         .itemOutputs('kubejs:energized_nether_star_shard')
         .duration(400)
         .EUt(2048);
