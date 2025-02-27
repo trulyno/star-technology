f3a15e29861e0e58885876e9e45abab1091c173e
73mm1n <128571861+73mm1n@users.noreply.github.com>
2025-02-27 21:30:49 +0300

    Removed template/example code

--- kubejs/server_scripts/HM/pre-lv/recipes.js
+++ kubejs/server_scripts/HM/pre-lv/recipes.js
@@ -558,24 +558,6 @@ ServerEvents.recipes(event => {
         ]).transitionalItem(inter).loops(8)
         });
 
-        event.recipes.create.sequenced_assembly([
-            Item.of('create:precision_mechanism').withChance(130.0), // this is the item that will appear in JEI as the result
-            Item.of('create:golden_sheet').withChance(8.0), // the rest of these items will be part of the scrap
-            Item.of('create:andesite_alloy').withChance(8.0),
-            Item.of('create:cogwheel').withChance(5.0),
-            Item.of('create:shaft').withChance(2.0),
-            Item.of('create:crushed_gold_ore').withChance(2.0),
-            Item.of('2x minecraft:gold_nugget').withChance(2.0),
-            'minecraft:iron_ingot',
-            'minecraft:clock'
-        ], 'create:golden_sheet', [ // 'create:golden_sheet' is the input
-            // the transitional item set by `transitionalItem('create:incomplete_large_cogwheel')` is the item used during the intermediate stages of the assembly
-            event.recipes.createDeploying('create:incomplete_precision_mechanism', ['create:incomplete_precision_mechanism', 'create:cogwheel']),
-            // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
-            event.recipes.createDeploying('create:incomplete_precision_mechanism', ['create:incomplete_precision_mechanism', 'create:large_cogwheel']),
-            event.recipes.createDeploying('create:incomplete_precision_mechanism', ['create:incomplete_precision_mechanism', 'minecraft:iron_nugget'])
-        ]).transitionalItem('create:incomplete_precision_mechanism').loops(5) //
-
     //Create Recipes
     event.shapeless(Item.of('gtceu:wood_plate', 2), [
         '#forge:tools/files','#minecraft:planks','#minecraft:planks'
