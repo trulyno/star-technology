2368ad641a704543250c1e80bc872b2e40c62a4c
KillLaAqua <KillLaAqua@gmail.com>
2025-03-03 06:50:34 -0600

    El Commented

--- kubejs/server_scripts/endgame content/nether_lines/estalt_line.js
+++ kubejs/server_scripts/endgame content/nether_lines/estalt_line.js
@@ -1,40 +1,47 @@
 
-ServerEvents.recipes(event => {
+// ServerEvents.recipes(event => {
 
-    event.recipes.gtceu.chemical_plant('estaltine_proc_1')
-        .itemInputs('2x gtceu:estaltine_dust', '2x gtceu:carbon_dust')
-        .inputFluids('gtceu:sulfuric_acid 8000')
-        .itemOutputs('4x gtceu:estaltine_sulfate_dust')
-        .outputFluids('gtceu:hydrogen_sulfide 1000', 'gtceu:ethane 1000')
-        .duration(600)
-        .EUt(100000);
+//     event.recipes.gtceu.distillation_tower('molten_estaltadyne_mixture')
+//         .inputFluids('gtceu:molten_estaltadyne_mixture 300000')
+//         .outputFluids('gtceu:estaltadyne 200000')
+//         .outputFluids('gtceu:highly_unstable_nether_magma 50000')
+//         .outputFluids('gtceu:molten_ore_mixture 50000')
+//         .itemOutputs('32x gtceu:netherrack_dust')
 
-    event.recipes.gtceu.chemical_plant('estaltine_proc_2')
-        .itemInputs('2x gtceu:estaltine_sulfate_dust')
-        .inputFluids('gtceu:hydrofluoric_acid 4000')
-        .itemOutputs('gtceu:estaltine_fluoride_dust')
-        .outputFluids('gtceu:sulfuric_acid 2000')
-        .duration(600)
-        .EUt(100000);
+//     event.recipes.gtceu.electrolyzer('estaltadyne_dust')
+//         .inputFluids('gtceu:estaltadyne 1000')
+//         .outputFluids('gtceu:mystical_nether_magma 250')
+//         .itemOutputs('gtceu:estaltadyne_dust','gtceu:small_estaltadyne_dust')
 
-    event.recipes.gtceu.chemical_plant('estaltine_proc_3')
-        .itemInputs('gtceu:estaltine_fluoride_dust', '4x gtceu:sodium_hydroxide_dust')
-        .itemOutputs('gtceu:estaltine_hydroxide_dust', '4x gtceu:sodium_fluoride_dust')
-        .duration(600)
-        .EUt(100000);
+//     event.recipes.gtceu.electric_blast_furnace('metmalic_estaltadyne_dust')
+//         .itemInputs('1x gtceu:estaltadyne_dust')
+//         .itemInputs('2x gtceu:carbon_dust')
+//         .itemOutputs('1x gtceu:metmalic_estaltadyne_dust')
+//         .outputFluids('gtceu:carbon_dioxide 2000')
+//         .blastFurnaceTemp(12280)
 
-    event.recipes.gtceu.electrolyzer('estaltine_proc_4')
-        .itemInputs('gtceu:estaltine_hydroxide_dust')
-        .itemOutputs('gtceu:estaltine_oxide_dust')
-        .outputFluids('minecraft:water 2000')
-        .duration(600)
-        .EUt(100000);
+//     event.recipes.gtceu.electrolyzer('magnemalic_estaltadyne_dust')
+//         .itemInputs('1x gtceu:metmalic_estaltadyne_dust')
+//         .itemOutputs('2x gtceu:aluminium_dust')
+//         .itemOutputs('1x gtceu:magnemalic_estaltadyne_dust')
 
-    event.recipes.gtceu.heat_chamber('estaltine_proc_5')
-        .itemInputs('gtceu:estaltine_oxide_dust', 'gtceu:carbon_dust')
-        .itemOutputs('gtceu:estalt_dust', 'gtceu:nether_rare_earth_dust')
-        .outputFluids('gtceu:carbon_dioxide 1000')
-        .duration(2400)
-        .EUt(100000);
+//     event.recipes.gtceu.chemical_plant('tytite_estaltadyne_dust')
+//         .itemInputs('1x gtceu:magnemalic_estaltadyne_dust')
+//         .itemInputs('15x gtceu:sodium_hydroxide_dust')
+//         .inputFluids('gtceu:oxygen 15000')
+//         .itemOutputs('1x gtceu:tytite_estaltadyne_dust')
 
-});
+//     event.recipes.gtceu.electric_blast_furnace('estaltadyne_hydride_dust')
+//         .itemInputs('1x gtceu:tytite_estaltadyne_dust')
+//         .inputFluids('gtceu:hydrofluoric_acid 9000')
+//         .itemOutputs('1x gtceu:estaltadyne_hydride_dust')
+//         .itemOutputs('12x gtceu:titanium_trifluoride_dust')
+//         .blastFurnaceTemp(13400)
+
+//     event.recipes.gtceu.large_chemical_reactor('estalt_dust')
+//         .itemInputs('1x gtceu:estaltadyne_hydride_dust')
+//         .itemInputs('15x gtceu:phosphate_dust')
+//         .itemOutputs('4x gtceu:estalt_dust')
+//         .outputFluids('gtceu:phosphoric_acid 3000')
+
+// });
