bb7bf7377ed7e7a27e76897371379cbecc440c81
KillLaAqua <KillLaAqua@gmail.com>
2025-03-03 17:59:15 -0600

    El fix + CtIs Looping

--- kubejs/server_scripts/endgame content/nether_lines/calamatium_isovol_line.js
+++ kubejs/server_scripts/endgame content/nether_lines/calamatium_isovol_line.js
@@ -4,13 +4,13 @@ ServerEvents.recipes(event => {
     event.recipes.gtceu.chemical_plant('estalt_dissolving')
         .itemInputs('6x gtceu:estalt_dust')
         .inputFluids('gtceu:fluoroantimonic_acid 2000')
-        .outputFluids('gtceu:impure_calamatium_solution 1000', 'gtceu:impure_isovol_solution 1000')
+        .outputFluids('gtceu:impure_calamatium_solution 1000', 'gtceu:impure_isovol_solution 1000', 'gtceu:fluorine 4000')
         .duration(1200)
         .EUt(100000);
 
     event.recipes.gtceu.centrifuge('impure_calamatium_solution')
         .inputFluids('gtceu:impure_calamatium_solution 1000')
-        .itemOutputs('gtceu:antimony_trifluoride_dust')
+        .itemOutputs('4x gtceu:antimony_trifluoride_dust')
         .outputFluids('gtceu:calamatium_solution 1000')
         .duration(600)
         .EUt(100000);
@@ -18,15 +18,7 @@ ServerEvents.recipes(event => {
     event.recipes.gtceu.centrifuge('calamatium_solution')
         .inputFluids('gtceu:calamatium_solution 1000')
         .itemOutputs('gtceu:calamatium_fluoride_dust')
-        .outputFluids('minecraft:water 500')
-        .duration(600)
-        .EUt(30000);
-
-    event.recipes.gtceu.chemical_reactor('calamatium_fluoride')
-        .itemInputs('gtceu:calamatium_fluoride_dust')
-        .inputFluids('gtceu:hydrogen 2000')
-        .itemOutputs('gtceu:calamatium_dust')
-        .outputFluids('gtceu:hydrofluoric_acid 2000')
+        .outputFluids('gtceu:hydrogen 2000')
         .duration(600)
         .EUt(30000);
 
@@ -40,7 +32,7 @@ ServerEvents.recipes(event => {
 
     event.recipes.gtceu.centrifuge('impure_isovol_solution')
         .inputFluids('gtceu:impure_isovol_solution 1000')
-        .itemOutputs('gtceu:antimony_trifluoride_dust')
+        .itemOutputs('4x gtceu:antimony_trifluoride_dust')
         .outputFluids('gtceu:isovol_solution 1000')
         .duration(600)
         .EUt(100000);
@@ -48,17 +40,10 @@ ServerEvents.recipes(event => {
     event.recipes.gtceu.centrifuge('isovol_solution')
         .inputFluids('gtceu:isovol_solution 1000')
         .itemOutputs('gtceu:isovol_fluoride_dust')
-        .outputFluids('minecraft:water 500')
+        .outputFluids('gtceu:hydrogen 2000')
         .duration(600)
         .EUt(30000);
 
-    event.recipes.gtceu.chemical_reactor('isovol_fluoride')
-        .itemInputs('gtceu:isovol_fluoride_dust')
-        .inputFluids('gtceu:hydrogen 2000')
-        .itemOutputs('gtceu:isovol_dust')
-        .outputFluids('gtceu:hydrofluoric_acid 2000')
-        .duration(600)
-        .EUt(30000);
 
     event.recipes.gtceu.large_chemical_reactor('isovol_fluoride')
         .itemInputs('gtceu:isovol_fluoride_dust')
