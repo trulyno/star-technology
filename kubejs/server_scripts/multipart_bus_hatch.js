25474ff8cbddc4f3d57f8f26afcd3e9726de6c5c
UltraPuPower1 <ultrapupower1@gmail.com>
2025-03-04 22:08:39 +0100

    multipart_bus_hatch refactor

--- kubejs/server_scripts/multipart_bus_hatch.js
+++ kubejs/server_scripts/multipart_bus_hatch.js
@@ -1,68 +1,29 @@
 // Readds Multipart bus/hatch recipes removed in 1.20.1 GT 
 // for ULV and LV tiers with parity to the GTceuM assembler recipes.
 ServerEvents.recipes(event => {
-    // ULV
-    // Busses
-    event.shaped(Item.of('gtceu:ulv_input_bus'), [
-        ' C ',
-        ' M ',
-        '   '], {
-        C: '#forge:chests/wooden',
-        M: 'gtceu:ulv_machine_hull'
-    });
-    event.shaped(Item.of('gtceu:ulv_output_bus'), [
-        '   ',
-        ' M ',
-        ' C '], {
-        C: '#forge:chests/wooden',
-        M: 'gtceu:ulv_machine_hull'
-    });
-    // Hatches
-    event.shaped(Item.of('gtceu:ulv_input_hatch'), [
-        ' G ',
-        ' M ',
-        '   '], {
-        G: '#forge:glass',
-        M: 'gtceu:ulv_machine_hull'
-    });
-    event.shaped(Item.of('gtceu:ulv_output_hatch'), [
-        '   ',
-        ' M ',
-        ' G '], {
-        G: '#forge:glass',
-        M: 'gtceu:ulv_machine_hull'
-    });
-
-    // LV
-    // Busses
-    event.shaped(Item.of('gtceu:lv_input_bus'), [
-        ' C ',
-        ' M ',
-        '   '], {
-        C: 'gtceu:wood_crate',
-        M: 'gtceu:lv_machine_hull'
-    });
-    event.shaped(Item.of('gtceu:lv_output_bus'), [
-        '   ',
-        ' M ',
-        ' C '], {
-        C: 'gtceu:wood_crate',
-        M: 'gtceu:lv_machine_hull'
-    });
-    // Hatches
-    event.shaped(Item.of('gtceu:lv_input_hatch'), [
-        ' G ',
-        ' M ',
-        '   '], {
-        G: 'gtceu:wood_drum',
-        M: 'gtceu:lv_machine_hull'
-    });
-    event.shaped(Item.of('gtceu:lv_output_hatch'), [
-        '   ',
-        ' M ',
-        ' G '], {
-        G: 'gtceu:wood_drum',
-        M: 'gtceu:lv_machine_hull'
+    [
+        {tier: 'ulv', item: '#forge:chests/wooden', fluid: '#forge:glass'},
+        {tier: 'lv', item: 'gtceu:wood_crate', fluid: 'gtceu:wood_drum'}
+    ].forEach(tier=>{
+        [
+            {type: 'input', shape: [' C ',' M ','   ']},
+            {type: 'output', shape: ['   ',' M ',' C ']}
+        ].forEach(type=> {
+            // Busses
+            event.shaped(Item.of(`gtceu:${tier.tier}_${type.type}_bus`),
+            type.shape, 
+            {
+                C: tier.item,
+                M: `gtceu:${tier.tier}_machine_hull`
+            });
+            // Hatches
+            event.shaped(Item.of(`gtceu:${tier.tier}_${type.type}_hatch`),
+            type.shape,
+            {
+                C: tier.fluid,
+                M: `gtceu:${tier.tier}_machine_hull`
+            });
+        });
     });
 
 });
