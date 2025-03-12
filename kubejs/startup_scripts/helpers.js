5705868267a722c8eee846c63c16d698422a0d56
73mm1n <128571861+73mm1n@users.noreply.github.com>
2025-03-12 20:46:28 +0300

    Update helpers.js

--- kubejs/startup_scripts/helpers.js
+++ kubejs/startup_scripts/helpers.js
@@ -6,19 +6,37 @@ const GTTankWidget = Java.loadClass('com.gregtechceu.gtceu.api.gui.widget.TankWi
 global.primitiveSlotFunction = (template, machine) => {
     return (io, slot, x, y) => {
     const ioChoice = (io === 'in') ? machine.importItems.storage : machine.exportItems.storage;
-    template.addWidget(new GTSlotWidget(ioChoice, slot, x, y, true, true).setBackground(GuiTextures.PRIMITIVE_SLOT));
+    const canput = (io === 'in') ? true : false ;
+    template.addWidget(new GTSlotWidget(ioChoice, slot, x, y, true, canput).setBackground(GuiTextures.PRIMITIVE_SLOT));
     }
-  }
+}
+  /*FOR PRIMITIVE ITEM WIDGET
+    itemWidget = global.primitiveSlotFunction = (template, machine)
+    itemWidget('in', 0, 52, 47);
+    itemWidget('out', 0, 104, 38);
+    itemWidget('in/out', <index>, posX, posY);
+  */
   
 global.finalizePrimitiveMulti = (template, machine) => {
     return (x, y, w, h) => {
       template.addWidget(new ProgressWidget(() => machine.getRecipeLogic().getProgressPercent(), x, y, w, h, GuiTextures.PRIMITIVE_BLAST_FURNACE_PROGRESS_BAR));
     }
 }
+  /*FOR PRIMITIVE PROGRESS WIDGET
+    const progressbar = global.finalizePrimitiveMulti(template, machine);
+    progressbar(77, 38, 20, 18);
+    progressbar(posX, posY, Width, Height);
+  */
 
 global.primitiveTankFunction = (template, machine) => {
     return (io, tank, x, y) => {
     const ioChoice = (io === 'in') ? machine.importFluids.getStorages()[tank] : machine.exportFluids.getStorages()[tank];
-    template.addWidget(new GTTankWidget(ioChoice, x, y, true, true).setBackground(new GuiTextureGroup(GuiTextures.PRIMITIVE_LARGE_FLUID_TANK,GuiTextures.PRIMITIVE_LARGE_FLUID_TANK_OVERLAY)).setCapacity(16));
+    const canput = (io === 'in') ? true : false ;
+    template.addWidget(new GTTankWidget(ioChoice, x, y, canput, true).setBackground(GuiTextures.PRIMITIVE_LARGE_FLUID_TANK));
     }
 }
+  /*FOR PRIMITIVE TANK WIDGET
+    const tankWidget = global.primitiveTankFunction(template, machine);
+    tankWidget('in', 0, 34, 47);
+    tankWidget('in/out', <index>, posX, posY);
+  */
