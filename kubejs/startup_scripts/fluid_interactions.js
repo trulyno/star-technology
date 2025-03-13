31d763ec2e3d75d2920235c054c4f377e2cf7ac4
VoxelError <61365087+Asterverse@users.noreply.github.com>
2025-03-13 14:09:19 -0700

    Disabled fluid interactions 😎

---
+++ kubejs/startup_scripts/fluid_interactions.js
@@ -0,0 +1,13 @@
+ForgeEvents.onEvent('net.minecraftforge.event.level.BlockEvent$FluidPlaceBlockEvent', event => {
+	// 	const result = event.getNewState().getBlock();
+	// 	let block;
+
+	// 	if (result == Blocks.COBBLESTONE) block = Blocks.CALCITE;
+	// 	if (result == Blocks.STONE) block = Blocks.TUFF;
+	// 	if (result == Blocks.BASALT) block = Blocks.DEEPSLATE;
+	// 	if (result == Blocks.OBSIDIAN) block = Blocks.BEDROCK;
+
+	// 	if (block) event.setNewState(block.defaultBlockState());
+
+	event.setNewState(event.getOriginalState())
+});
