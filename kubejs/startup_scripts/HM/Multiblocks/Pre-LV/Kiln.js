aa3dd94af1d52f077a682d2f46faefb131d51c42
VoxelError <61365087+Asterverse@users.noreply.github.com>
2025-03-14 19:53:26 -0700

    Re-did UI helper Added UI for Kiln & Rugged Alloyer

--- kubejs/startup_scripts/HM/Multiblocks/Pre-LV/Kiln.js
+++ kubejs/startup_scripts/HM/Multiblocks/Pre-LV/Kiln.js
@@ -1,6 +1,3 @@
-const EditableMachineUI = Java.loadClass('com.gregtechceu.gtceu.api.gui.editor.EditableMachineUI')
-const GTSlotWidget = Java.loadClass('com.gregtechceu.gtceu.api.gui.widget.SlotWidget')
-
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
 	event.create('kiln')
 		.category('kiln')
@@ -9,38 +6,38 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
 });
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
-	const builder = event.create('gtceu:kiln', 'primitive');
-
-	builder.rotationState(RotationState.NON_Y_AXIS);
-	builder.recipeType('kiln');
-	builder.pattern(definition => FactoryBlockPattern.start()
-		.aisle("BBB", "BBB", "BBB", "#B#", "#A#")
-		.aisle("BBB", "BCB", "B#B", "B#B", "A#A")
-		.aisle("BBB", "BDB", "BBB", "#B#", "#A#")
-		.where("A", Predicates.blocks('minecraft:stripped_jungle_wood'))
-		.where("B", Predicates.blocks("minecraft:mud_bricks"))
-		.where("#", Predicates.any())
-		.where("C", Predicates.blocks("minecraft:campfire"))
-		.where("D", Predicates.controller(Predicates.blocks(definition.get())))
-		.build()
-	);
-	builder.workableCasingRenderer("minecraft:block/mud_bricks", "gtceu:block/multiblock/primitive_blast_furnace", false);
-
-	builder.editableUI(
-		new EditableMachineUI(
-			'primitive',
-			'gtceu:kiln',
-			() => new WidgetGroup(),
-			(template, machine) => {
-				template.setSize(166, 100)
-				template.setBackground(GuiTextures.PRIMITIVE_BACKGROUND)
-
-				template.addWidget(new GTSlotWidget(machine.importItems.storage, 0, 52, 29, true, true).setBackground(GuiTextures.PRIMITIVE_SLOT));
-				template.addWidget(new GTSlotWidget(machine.importItems.storage, 1, 52, 47, true, true).setBackground(GuiTextures.PRIMITIVE_SLOT));
-				template.addWidget(new GTSlotWidget(machine.exportItems.storage, 0, 104, 38, true, false).setBackground(GuiTextures.PRIMITIVE_SLOT));
-
-				template.addWidget(new ProgressWidget(() => machine.getRecipeLogic().getProgressPercent(), 77, 38, 20, 18, GuiTextures.PRIMITIVE_BLAST_FURNACE_PROGRESS_BAR))
-			}
-		)
-	);
+	event.create('gtceu:kiln', 'primitive')
+		.rotationState(RotationState.NON_Y_AXIS)
+		.recipeType('kiln')
+		.pattern(definition => FactoryBlockPattern.start()
+			.aisle("BBB", "BBB", "BBB", "#B#", "#A#")
+			.aisle("BBB", "BCB", "B#B", "B#B", "A#A")
+			.aisle("BBB", "BDB", "BBB", "#B#", "#A#")
+			.where("A", Predicates.blocks('minecraft:stripped_jungle_wood'))
+			.where("B", Predicates.blocks("minecraft:mud_bricks"))
+			.where("#", Predicates.any())
+			.where("C", Predicates.blocks("minecraft:campfire"))
+			.where("D", Predicates.controller(Predicates.blocks(definition.get())))
+			.build())
+		.workableCasingRenderer("minecraft:block/mud_bricks", "gtceu:block/multiblock/primitive_blast_furnace", false)
+		.editableUI(
+			global.ui_helper({
+				group: 'primitive',
+				name: 'kiln',
+				size: [166, 100],
+				background: GuiTextures.PRIMITIVE_BACKGROUND,
+				progress: {
+					pos: [77, 38],
+					size: [20, 18],
+					texture: GuiTextures.PRIMITIVE_BLAST_FURNACE_PROGRESS_BAR
+				},
+				inputs: [
+					{ type: 'item', index: 0, pos: [52, 29], texture: GuiTextures.PRIMITIVE_SLOT },
+					{ type: 'item', index: 1, pos: [52, 47], texture: GuiTextures.PRIMITIVE_SLOT },
+				],
+				outputs: [
+					{ type: 'item', index: 0, pos: [104, 38], texture: GuiTextures.PRIMITIVE_SLOT },
+				]
+			})
+		);
 });
--- kubejs/startup_scripts/HM/Multiblocks/Pre-LV/Rugged Alloyer.js
+++ kubejs/startup_scripts/HM/Multiblocks/Pre-LV/Rugged Alloyer.js
@@ -1,61 +1,43 @@
-// // const $UI = Java.loadClass('com.gregtechceu.gtceu.common.machine.multiblock.primitive.PrimitiveWorkableMachine');
-// // const items = Java.loadClass('com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler');
-// // const fluids = Java.loadClass('com.gregtechceu.gtceu.api.machine.trait.NotifiableFluidTank');
-// const $ModularUI = Java.loadClass('com.lowdragmc.lowdraglib.gui.modular.ModularUI');
-// const $LabelWidget = Java.loadClass('com.lowdragmc.lowdraglib.gui.widget.LabelWidget');
-// const $ProgressWidget = Java.loadClass('com.lowdragmc.lowdraglib.gui.widget.ProgressWidget');
-// const $GuiTextureGroup = Java.loadClass('com.lowdragmc.lowdraglib.gui.texture.GuiTextureGroup');
-
-// const $SlotWidget = Java.loadClass('com.gregtechceu.gtceu.api.gui.widget.SlotWidget');
-// const $UITemplate = Java.loadClass('com.gregtechceu.gtceu.api.gui.UITemplate');
-
-// function createUI(entityPlayer) {
-//     return new $ModularUI(176, 166, this, entityPlayer)
-//         .background(GuiTextures.PRIMITIVE_BACKGROUND)
-//         .widget(new $LabelWidget(5, 5, getBlockState().getBlock().getDescriptionId()))
-//         .widget(new $SlotWidget(importItems.storage, 0, 52, 20, true, true)
-//             .setBackgroundTexture(
-//                 new $GuiTextureGroup(GuiTextures.PRIMITIVE_SLOT, GuiTextures.PRIMITIVE_INGOT_OVERLAY)))
-//         .widget(new $SlotWidget(importItems.storage, 1, 52, 38, true, true)
-//             .setBackgroundTexture(
-//                 new $GuiTextureGroup(GuiTextures.PRIMITIVE_SLOT, GuiTextures.PRIMITIVE_DUST_OVERLAY)))
-//         .widget(new $SlotWidget(importItems.storage, 2, 52, 56, true, true)
-//             .setBackgroundTexture(
-//                 new $GuiTextureGroup(GuiTextures.PRIMITIVE_SLOT, GuiTextures.PRIMITIVE_FURNACE_OVERLAY)))
-//         .widget(new $ProgressWidget(() => recipeLogic.getProgressPercent(), 77, 39, 20, 15,
-//             GuiTextures.PRIMITIVE_BLAST_FURNACE_PROGRESS_BAR))
-//         .widget(new $SlotWidget(exportItems.storage, 0, 104, 38, true, false)
-//             .setBackgroundTexture(
-//                 new $GuiTextureGroup(GuiTextures.PRIMITIVE_SLOT, GuiTextures.PRIMITIVE_INGOT_OVERLAY)))
-//         .widget(new $SlotWidget(exportItems.storage, 1, 122, 38, true, false)
-//             .setBackgroundTexture(
-//                 new $GuiTextureGroup(GuiTextures.PRIMITIVE_SLOT, GuiTextures.PRIMITIVE_DUST_OVERLAY)))
-//         .widget(new $SlotWidget(exportItems.storage, 2, 140, 38, true, false)
-//             .setBackgroundTexture(
-//                 new $GuiTextureGroup(GuiTextures.PRIMITIVE_SLOT, GuiTextures.PRIMITIVE_DUST_OVERLAY)))
-//         .widget($UITemplate.bindPlayerInventory(entityPlayer.getInventory(), GuiTextures.PRIMITIVE_SLOT, 7, 84,
-//             true));
-// };
-
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
-    event.create('rugged_alloyer')
-        .category('rugged_alloyer')
-        .setMaxIOSize(3, 2, 0, 0)
-        .setSound(GTSoundEntries.FURNACE)
+	event.create('rugged_alloyer')
+		.category('rugged_alloyer')
+		.setMaxIOSize(3, 2, 0, 0)
+		.setSound(GTSoundEntries.FURNACE)
 });
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
-    event.create('rugged_alloyer', 'multiblock')
-        .rotationState(RotationState.NON_Y_AXIS)
-        .recipeType('rugged_alloyer')
-		.pattern(definition => FactoryBlockPattern.start()	
-            .aisle("AAA", "AAA", "AAA", "AAA") 	
-            .aisle("AAA", "A#A", "A#A", "A#A") 	
-            .aisle("AAA", "ABA", "AAA", "AAA") 	
-            .where("A", Predicates.blocks("kubejs:reinforced_stone_bricks"))	
-            .where("#", Predicates.blocks("minecraft:air"))	
-            .where("B", Predicates.controller(Predicates.blocks(definition.get())))
+	event.create('rugged_alloyer', 'primitive')
+		.rotationState(RotationState.NON_Y_AXIS)
+		.recipeType('rugged_alloyer')
+		.pattern(definition => FactoryBlockPattern.start()
+			.aisle("AAA", "AAA", "AAA", "AAA")
+			.aisle("AAA", "A#A", "A#A", "A#A")
+			.aisle("AAA", "ABA", "AAA", "AAA")
+			.where("A", Predicates.blocks("kubejs:reinforced_stone_bricks"))
+			.where("#", Predicates.blocks("minecraft:air"))
+			.where("B", Predicates.controller(Predicates.blocks(definition.get())))
 			.build())
-		.workableCasingRenderer("kubejs:block/hm/reinforced_stone_bricks",
-			"gtceu:block/machines/electric_furnace", false);
+		.workableCasingRenderer("kubejs:block/hm/reinforced_stone_bricks", "gtceu:block/machines/electric_furnace", false)
+		.editableUI(
+			global.ui_builder({
+				group: 'primitive',
+				name: 'primitive_ore_factory',
+				size: [166, 100],
+				background: GuiTextures.PRIMITIVE_BACKGROUND,
+				progress: {
+					pos: [77, 38],
+					size: [20, 18],
+					texture: GuiTextures.PRIMITIVE_BLAST_FURNACE_PROGRESS_BAR
+				},
+				inputs: [
+					{ type: 'item', index: 0, pos: [34, 29], texture: GuiTextures.PRIMITIVE_SLOT },
+					{ type: 'item', index: 1, pos: [52, 29], texture: GuiTextures.PRIMITIVE_SLOT },
+					{ type: 'item', index: 2, pos: [70, 29], texture: GuiTextures.PRIMITIVE_SLOT },
+				],
+				outputs: [
+					{ type: 'item', index: 0, pos: [104, 29], texture: GuiTextures.PRIMITIVE_SLOT },
+					{ type: 'item', index: 1, pos: [122, 29], texture: GuiTextures.PRIMITIVE_SLOT },
+				],
+			})
+		)
 });
