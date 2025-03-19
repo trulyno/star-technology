f40d11e54eaad9bb0f65079787bec81c647aabaa
73mm1n <128571861+73mm1n@users.noreply.github.com>
2025-03-19 09:42:38 +0300

    added recipetype to primitive mixer and assembler

--- kubejs/startup_scripts/HM/Single Block Machines/ulv_gt_machines.js
+++ kubejs/startup_scripts/HM/Single Block Machines/ulv_gt_machines.js
@@ -4,11 +4,19 @@ if (CommonProperties.get().packMode == 'hard' || CommonProperties.get().packMode
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('mixer', 'simple')
         .tiers(GTValues.ULV)
+        .definition((tier, builder) =>
+            builder
+                .recipeType('mixer')
+          )
 });
 
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('assembler', 'simple')
         .tiers(GTValues.ULV)
+        .definition((tier, builder) =>
+            builder
+                .recipeType('assembler')
+          )
 });
 
 };//if end
