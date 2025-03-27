39fc5c6cd643fcd5536cd629816028fd3cea64d9
VoxelError <61365087+Asterverse@users.noreply.github.com>
2025-03-26 22:09:53 -0700

    Re-did LootJS syntax and reduced sapling/cocoa bean rates

--- kubejs/server_scripts/HM/LootJS.js
+++ kubejs/server_scripts/HM/LootJS.js
@@ -5,40 +5,34 @@ if (CommonProperties.get().packMode == 'hard' || CommonProperties.get().packMode
 		event
 			.addBlockLootModifier('minecraft:dead_bush')
 			.removeLoot(Ingredient.all)
-			.addWeightedLoot(
-				[1, 2],
-				[Item.of('minecraft:stick').withChance(100)]
-			);
-
+			.addLoot('minecraft:stick')
+			.limitCount([1, 2]);
 		event
 			.addBlockLootModifier('minecraft:dead_bush')
 			.matchMainHand(Item.of('gtceu:flisnt_knife'))
 			.removeLoot(Ingredient.all)
-			.addWeightedLoot(
-				[2, 3],
-				[Item.of('farmersdelight:straw').withChance(100)]
-			);
-
+			.addLoot('farmersdelight:straw')
+			.limitCount([2, 3]);
 		event
 			.addBlockLootModifier('minecraft:campfire')
 			.removeLoot(Ingredient.all)
-			.addWeightedLoot(
-				[3, 10],
-				[Item.of('gtceu:ash_dust').withChance(50), Item.of('farmersdelight:tree_bark').withChance(25)]
+			.addLoot(
+				LootEntry.of('gtceu:ash_dust').limitCount([2, 3]),
+				LootEntry.of('farmersdelight:tree_bark').limitCount([0, 1]),
 			);
-
-		event
-			.addBlockLootModifier(`minecraft:jungle_leaves`)
-			.matchMainHand(Item.of('exnihilosequentia:wooden_crook'))
-			.addWeightedLoot([Item.of('minecraft:cocoa_beans').withChance(15)]);
+		// .addWeightedLoot(
+		// 	[3, 10],
+		// 	[Item.of('gtceu:ash_dust').withChance(50), Item.of('farmersdelight:tree_bark').withChance(25)]
+		// );
 
 		['oak', 'spruce', 'birch', 'dark_oak', 'jungle', 'acacia'].forEach(tree => {
 			event
 				.addBlockLootModifier(`minecraft:${tree}_leaves`)
+				.removeLoot(Ingredient.all)
 				.matchMainHand(Item.of('exnihilosequentia:wooden_crook'))
-				.addWeightedLoot(
-					[0, 1],
-					[Item.of(`minecraft:${tree}_sapling`).withChance(50)]
+				.addLoot(
+					LootEntry.of(`minecraft:${tree}_sapling`).when(c => c.randomChance(0.15)),
+					(tree == 'jungle') && LootEntry.of('minecraft:cocoa_beans').when(c => c.randomChance(0.01)),
 				);
 		});
 	});
