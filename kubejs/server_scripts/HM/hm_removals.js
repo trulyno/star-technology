b514adcfdcba05f0101b34d14ff4a1e088973348
VoxelError <61365087+Asterverse@users.noreply.github.com>
2025-03-29 11:06:18 -0700

    Wire cutter fix + some tidying

--- kubejs/server_scripts/HM/hm_removals.js
+++ kubejs/server_scripts/HM/hm_removals.js
@@ -1,45 +1,52 @@
 // packmode: hard
 
 ServerEvents.recipes(event => {
-
-	const toRemoveOutputHM = [
-		'exnihilosequentia:silkworm', '#exnihilosequentia:crook', '#exnihilosequentia:hammer'
-	];
-
-	const toRemoveIdHM = [
-		/^gtceu:.*flisnt.*/, /^gtceu:shapeless.*_planks/, /^exnihilosequentia:crushing.*/, /^gtceu:shaped.plate/,
-		//[^(?!^rubber$)] <- rubber sheet recipe is kjs and other rubbers is solidifier and bender only, wood is compressor for treated and crafting for regular
-		/^gtceu:shaped.ring.*[^(?!^rubber$)]/, /^gtceu:shaped.stick.*[^(?!^.*_rubber_.*$)]/,
+	[
+		/^gtceu:.*flisnt.*/,
+		/^gtceu:shapeless.*_planks/,
+		/^exnihilosequentia:crushing.*/,
+		/^gtceu:shaped.plate/,
+		// [^(?!^rubber$)] <- rubber sheet recipe is kjs and other rubbers is solidifier and bender only, wood is compressor for treated and crafting for regular
+		/^gtceu:shaped.ring.*[^(?!^rubber$)]/,
+		/^gtceu:shaped.stick.*[^(?!^.*_rubber_.*$)]/,
 		// poly rubbers and other rubbers are being wierd but you have solidifier by the time you get to pvc/ptfe so :3
-		/^gtceu:shaped.*wire//*_.*[^(?!^cutter$)]*/, /^gtceu:shapeless.fine.*/, /^gtceu:shaped.screw_.*/, /^gtceu:shaped.*gear.*/, /^gtceu:shaped.bolt.*/, /^gtceu:shaped.rotor.*/, /^gtceu:shaped.spring.*/, /^gtceu:shaped.*pipe.*/, /^gtceu:shaped.foil.*/, /^gtceu:shapeless.*cable.*/, /^gtceu:shaped.buzzsaw.*/, /^gtceu:shaped.round.*/,
-		//plates -> single and double, stick -> long and short rod, gear -> both small and normal, pipe -> exclusion of quad and non dont work, all pipes + constricting, cable-> just a sweep on all cable crafting table recipie
-		'create:crafting/kinetics/shaft', /^create:crafting.*cogwheel.*/, 'create:crafting/kinetics/belt_connector', /^farmersdelight:.*knife/, 'minecraft:crafting_table'
-	];
-	const toRemoveTypeHM = [
-		'minecraft:enchantment', 'exnihilosequentia:sifting'
-	];
+		/^gtceu:shaped\/.*_wire_single/,
+		/^gtceu:shapeless.fine.*/,
+		/^gtceu:shaped.screw_.*/,
+		/^gtceu:shaped.*gear.*/,
+		/^gtceu:shaped.bolt.*/,
+		/^gtceu:shaped.rotor.*/,
+		/^gtceu:shaped.spring.*/,
+		/^gtceu:shaped.*pipe.*/,
+		/^gtceu:shaped.foil.*/,
+		/^gtceu:shapeless.*cable.*/,
+		/^gtceu:shaped.buzzsaw.*/,
+		/^gtceu:shaped.round.*/,
+		// plates -> single and double, stick -> long and short rod, gear -> both small and normal, pipe -> exclusion of quad and non dont work, all pipes + constricting, cable-> just a sweep on all cable crafting table recipie
+		/^farmersdelight:.*knife/,
+		/^create:crafting.*cogwheel.*/,
+		'create:crafting/kinetics/shaft',
+		'create:crafting/kinetics/belt_connector',
+		'minecraft:crafting_table',
+		'gtceu:shaped/iron_wire_single'
+	].forEach(id => event.remove({ id: id }));
 
-	const toRemoveTypeInputHM = [
-		['exnihilosequentia:harvest', '#minecraft:leaves'], ['exnihilosequentia:harvest', 'exnihilosequentia:infested_leaves'], //removes silkworm obtainability
+	// removes silkworm obtainability
+	[
+		'#minecraft:leaves',
+		'exnihilosequentia:infested_leaves',
+	].forEach(input => event.remove({ type: 'exnihilosequentia:harvest', input: input }));
 
+	[
+		'exnihilosequentia:silkworm',
+		'#exnihilosequentia:crook',
+		'#exnihilosequentia:hammer',
+	].forEach(output => event.remove({ output: output }));
 
-	];
-
-	toRemoveOutputHM.forEach(element => {
-		event.remove({ output: element });
-	});
-
-	toRemoveIdHM.forEach(element => {
-		event.remove({ id: element });
-	});
-
-	toRemoveTypeHM.forEach(element => {
-		event.remove({ type: element });
-	});
-
-	toRemoveTypeInputHM.forEach(element => {
-		event.remove({ type: element[0], input: element[1] });
-	});
+	[
+		'minecraft:enchantment',
+		'exnihilosequentia:sifting',
+	].forEach(type => event.remove({ type: type }));
 
 	event.remove({ input: /^exnihilosequentia:.*_pebble/ });
 
