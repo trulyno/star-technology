709660ba1779cc45f8b6cde1c4026679d65e7d61
trulyno <78795220+trulyno@users.noreply.github.com>
2025-04-16 12:18:08 +0300

    updated geode tooltips

--- kubejs/startup_scripts/items/geodes.js
+++ kubejs/startup_scripts/items/geodes.js
@@ -5,6 +5,7 @@ StartupEvents.registry('item', event => {
         ,`Blue_Topaz`,`Apatite`, `Monazite`].forEach(type => {
         event.create(`${type.toLowerCase()}_geode`)
             .displayName(`${type.replace(/_/, ' ')} Geode`)
-            .texture(`kubejs:item/resource_gen/geodes/cracked_${type.toLowerCase()}_geode`);
+            .texture(`kubejs:item/resource_gen/geodes/cracked_${type.toLowerCase()}_geode`)
+            .tooltip(`A simingly ordinary rock which hides a beatuful §b${type.toLowerCase().replace(/_/, ' ')}§r inside`);
         });
 });
