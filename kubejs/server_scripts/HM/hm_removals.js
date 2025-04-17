19019312c008c1046f626da4f6d171ac87dfa6d1
VoxelError <61365087+Asterverse@users.noreply.github.com>
2025-04-17 14:59:40 -0700

    Fixed Bulk Blasting removals

--- kubejs/server_scripts/HM/hm_removals.js
+++ kubejs/server_scripts/HM/hm_removals.js
@@ -52,10 +52,12 @@ ServerEvents.recipes(event => {
 	event.remove({ input: /^exnihilosequentia:.*_pebble/ });
 	event.remove({ output: 'exnihilosequentia:unfired_crucible' });
 
-	// event.forEachRecipe([{ type: 'minecraft:smelting' }, { type: 'minecraft:blasting' }], recipe => {
-	// 	event.remove({ id: recipe.getId() });
-	// 	event.custom(recipe.json).id(recipe.getId() + '_manual_only');
-	// }); Cant do this it screws ALL removal recipes
-
 	event.replaceInput({ input: 'minecraft:string' }, 'minecraft:string', '#forge:string');
+})
+
+ServerEvents.afterRecipes(event => {
+	event.forEachRecipe([{ type: 'minecraft:smelting' }, { type: 'minecraft:blasting' }], recipe => {
+		event.remove({ id: recipe.getId() });
+		event.custom(recipe.json).id(recipe.getId() + '_manual_only');
+	});
 })
