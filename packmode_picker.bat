98ed4be2868f2876e67a500fd27d6188280a9eeb
KillLaAqua <KillLaAqua@gmail.com>
2025-04-19 16:12:58 -0500

    Abydos Packmode

--- packmode_picker.bat
+++ packmode_picker.bat
@@ -6,6 +6,7 @@ setlocal
 
 set default_path=%~dp0packmode\default
 set hard_path=%~dp0packmode\hard
+set abydos_path=%~dp0packmode\abydos
 set target_path=%~dp0\
 
 color 0B
@@ -19,10 +20,12 @@ echo =======================================================
 echo.
 echo [D] Default - the standard experience
 echo [H] Hard - for the masochistic
+echo [A] Abydos - dehydration enjoyer
 echo.
 
-choice /c DH /n /m "Enter Packmode (D or H):"
+choice /c DHA /n /m "Enter Packmode (D H or A):"
 
+if errorlevel 3 goto set_abydos
 if errorlevel 2 goto set_hard
 if errorlevel 1 goto set_default
 
@@ -44,6 +47,16 @@ echo ===========================================================================
 echo =======================================================================================
 rmdir /s /q "%target_path%config\ftbquests\quests\chapters"
 robocopy "%hard_path%" "%target_path%" *.* /e /nfl /ndl
+goto end
+
+:set_abydos
+echo.
+echo Applying Packmode [Abydos]...
+echo.
+echo =======================================================================================
+echo =======================================================================================
+rmdir /s /q "%target_path%config\ftbquests\quests\chapters"
+robocopy "%abydos_path%" "%target_path%" *.* /e /nfl /ndl
 goto end
 
 :end
