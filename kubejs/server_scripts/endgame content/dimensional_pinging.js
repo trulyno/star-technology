10620172a2c56f8fe5147438abb23883d81cc916
KillLaAqua <KillLaAqua@gmail.com>
2025-04-18 21:04:52 -0500

    Adjusted UX

--- kubejs/server_scripts/endgame content/dimensional_pinging.js
+++ kubejs/server_scripts/endgame content/dimensional_pinging.js
@@ -110,12 +110,14 @@ const crystalfeed = (realmId, realm, stage, message) => {
     ItemEvents.rightClicked(`kubejs:${realm}_coordinate_crystal`, event => {
         if (event.player.isCrouching()) {
             event.item.count--
-            event.server.runCommandSilent(`execute as ${event.player.username} run give ${event.player.username} kubejs:blank_coordinate_crystal`);
-            event.server.runCommandSilent(`execute as ${event.player.username} run playsound minecraft:entity.player.levelup player ${event.player.username} ~ ~ ~`);
-            event.player.tell(`As you consume the echoes of the coordinate crystal, you hear voices whispering, and strange numbers appear before your eyes, along with visions of ${message}.`);
-            event.player.tell('');
-            event.server.runCommand(`execute as ${event.player.username} run sgjourney stargateNetwork address ${realmId}:${realm}`);
-            event.server.runCommandSilent(`execute as ${event.player.username} run gamestage add ${event.player.username} ${stage}`);
+            event.server.runCommandSilent(`give ${event.player.username} kubejs:coordinate_crystal`)
+            event.server.runCommandSilent(`execute at ${event.player.username} run playsound minecraft:block.enchantment_table.use player ${event.player.username} ~ ~ ~`);
+            event.server.scheduleInTicks(15, ctx => {
+                event.player.tell(`As you consume the echoes of the coordinate crystal, you hear voices whispering, and strange numbers appear before your eyes, along with visions of ${message}.`);
+                event.server.runCommand(`execute as ${event.player.username} run sgjourney stargateNetwork address ${realmId}:${realm}`);
+                event.server.runCommandSilent(`execute as ${event.player.username} run gamestage add ${event.player.username} ${stage}`);
+                event.server.runCommandSilent(`execute at ${event.player.username} run playsound sgjourney:milky_way_chevron_encode player ${event.player.username} ~ ~ ~`);
+            })
         }
     });
 }
