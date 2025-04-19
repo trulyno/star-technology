d0605db01dea0378629ea8a2ec975f5fdef89dbc
UltraPuPower1 <ultrapupower1@gmail.com>
2025-04-19 20:58:49 +0200

    mech sieve no fluids

--- kubejs/startup_scripts/multiblocks/resource_production/large sieve/mechanical_sieve.js
+++ kubejs/startup_scripts/multiblocks/resource_production/large sieve/mechanical_sieve.js
@@ -23,8 +23,6 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
             .where('W', Predicates.blocks('kubejs:wood_casing')
                 .or(Predicates.abilities(PartAbility.IMPORT_ITEMS).setMaxGlobalLimited(2).setPreviewCount(1))
                 .or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setMaxGlobalLimited(2).setPreviewCount(1))
-                .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(2).setPreviewCount(1))
-                .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setMaxGlobalLimited(2).setPreviewCount(1))
                 .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(1)))
             .where('F', Predicates.blocks('gtceu:treated_wood_frame'))     
             .where('M', Predicates.blocks('kubejs:meshblock'))
