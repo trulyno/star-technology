f02a6beeb147248dacdc044774b916b79cc49ea8
KillLaAqua <KillLaAqua@gmail.com>
2025-04-24 09:44:52 -0500

    Fixes: - Multi's not OC correctly

--- kubejs/startup_scripts/multiblocks/resource_production/molten_destabalizer.js
+++ kubejs/startup_scripts/multiblocks/resource_production/molten_destabalizer.js
@@ -10,7 +10,7 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('molten_destabilizer', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
         .recipeType('molten_destabilizing')
-        .recipeModifiers([GTRecipeModifiers.OC_NON_PERFECT, GTRecipeModifiers.PARALLEL_HATCH])
+        .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH, GTRecipeModifiers.OC_NON_PERFECT])
         .appearanceBlock(GCYMBlocks.CASING_HIGH_TEMPERATURE_SMELTING)
 		.pattern(definition => FactoryBlockPattern.start()
 			.aisle('   FFF   ','         ','         ','         ','         ','   BBB   ','         ','         ','         ','         ')
